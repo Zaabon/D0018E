e931164f8e05327c3f31659f948532314e1fd6bf
Zaabon <olof_ftw@hotmail.com>
2017-11-10 13:39:02 +0100

    Add files via upload

---
+++ product.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<?php include 'connect.php';?>
+
+<meta charset="utf-8">
+<title>Test</title>
+
+
+<link href="stylesheat.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+</body>
+
+</html>
