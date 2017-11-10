d798108181b56eeceb2f18171f765dee3ea4af3f
emelieakerstrom <emelie.akerstrom@live.se>
2017-11-10 16:16:55 +0100

    Update product.php

--- product.php
+++ product.php
@@ -12,6 +12,50 @@
 </head>
 
 <body>
+  <div id="product">
+		<h1>
+      <?php
+			  $sql = "SELECT name FROM Articles where name='".$_POST['name']."'";
+			  $row = $conn->query($sql);
+			
+			  foreach ($conn->query($sql) as $r){
+				  echo $r[0];
+			  }
+		  ?>
+		</h1>
+		<p>
+			Price:
+			<?php
+			  $sql = "SELECT price FROM Articles where name='".$_POST['name']."'";
+			  $row = $conn->query($sql);
+			
+			  foreach ($conn->query($sql) as $r){
+				  echo $r[0];
+			  }
+		  ?>
+		</p>
+		<p>
+			In stock:
+			<?php
+			  $sql = "SELECT stock FROM Articles where name='".$_POST['name']."'";
+			  $row = $conn->query($sql);
+			
+			  foreach ($conn->query($sql) as $r){
+				  echo $r[0];
+			  }
+		  ?>
+		</p>
+		<p>
+			Description:
+			<?php
+			  $sql = "SELECT description FROM Articles where name='".$_POST['name']."'";
+			  $row = $conn->query($sql);
+			
+			  foreach ($conn->query($sql) as $r){
+				  echo $r[0];
+			  }
+		  ?>
+		</p>
 </body>
 
 </html>
